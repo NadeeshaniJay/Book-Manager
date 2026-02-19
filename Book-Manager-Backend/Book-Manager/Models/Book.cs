6152bd309f0f11bfaa34c0fe3a59062cef8aeeba
nadeeshani00jay@gmail.com <139541836+NadeeshaniJay@users.noreply.github.com>
2026-02-19 12:34:57 +0530

    create book model

---
+++ Book-Manager-Backend/Book-Manager/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Book_Manager.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public string? Isbn { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
